a2771f068d75c64dde5fe46bc5b99e077534c207
Juan Sebastian <jsebastianlu16@gmail.com>
2023-03-28 15:07:37 -0500

    Se realizo decoracion para que le admin pueda registrar Clintes, Doctores y Enefermeros de momento

--- HostPital/core/admin.py
+++ HostPital/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Doctor, Cliente, Enfermero, Especialidad
 
-# Register your models here.
+@admin.register(Doctor)
+class doctorAd(admin.ModelAdmin):
+    list_display = ('id', 'tipoUsuario', 'nombre', 'apellido', 'numeroDocumento', 'fechaNacimiento', 'telefono', 'disponible')
+
+@admin.register(Enfermero)
+class enfermeroAd(admin.ModelAdmin):
+    list_display = ('id', 'tipoUsuario', 'nombre', 'apellido', 'numeroDocumento', 'fechaNacimiento', 'telefono', 'activo')
+
+@admin.register(Cliente)
+class clienteAd(admin.ModelAdmin):
+    list_display = ('id', 'tipoUsuario', 'nombre', 'apellido', 'numeroDocumento', 'fechaNacimiento', 'telefono', 'activo')
+
+# @admin.register(DoctorEspecialista())
+# class doctorEspAd(admin.ModelAdmin):
+#     list_display = ('id', 'doctor', 'especialidad')
