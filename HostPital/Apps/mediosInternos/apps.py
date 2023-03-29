262661cce14731a76e43906855ba1dab09ee3973
Juan Sebastian <jsebastianlu16@gmail.com>
2023-03-28 19:23:05 -0500

    Se le implemento un Verbose_name para modificar el titulo en administracion

--- HostPital/Apps/mediosInternos/apps.py
+++ HostPital/Apps/mediosInternos/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MediosInternosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Apps.mediosInternos'
+    verbose_name = 'Medios internos'
