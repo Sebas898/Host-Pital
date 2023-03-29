ca4901e44cd7bdb7739ca30611b6abb5afb47df6
Juan Sebastian <jsebastianlu16@gmail.com>
2023-03-28 21:35:59 -0500

    Se agrego filtros, barra de busqueda y fecha de creacion y actualizcion

--- HostPital/Apps/core/admin.py
+++ HostPital/Apps/core/admin.py
@@ -4,11 +4,20 @@ from .models import Doctor, Cliente, Enfermero
 @admin.register(Doctor)
 class doctorAd(admin.ModelAdmin):
     list_display = ('id', 'tipoUsuario', 'nombre', 'apellido', 'numeroDocumento', 'fechaNacimiento', 'telefono', 'disponible')
-
+    list_filter = ('id', 'tipoUsuario', 'numeroDocumento')
+    search_fields = ('id', 'numeroDocumento', 'tipoUsuario')
+    readonly_fields = ('created','updated',)
+    
 @admin.register(Enfermero)
 class enfermeroAd(admin.ModelAdmin):
     list_display = ('id', 'tipoUsuario', 'nombre', 'apellido', 'numeroDocumento', 'fechaNacimiento', 'telefono', 'activo')
+    list_filter = ('id', 'tipoUsuario', 'numeroDocumento')
+    search_fields = ('id', 'numeroDocumento', 'tipoUsuario')
+    readonly_fields = ('created','updated',)
 
 @admin.register(Cliente)
 class clienteAd(admin.ModelAdmin):
     list_display = ('id', 'tipoUsuario', 'nombre', 'apellido', 'numeroDocumento', 'fechaNacimiento', 'telefono', 'activo')
+    list_filter = ('id', 'tipoUsuario', 'numeroDocumento')
+    search_fields = ('id', 'numeroDocumento', 'tipoUsuario')
+    readonly_fields = ('created','updated',)
